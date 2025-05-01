39ce66a899b968226280b123d02045df98e6c144
Mark Simmons <mjsimmons@ucsd.edu>
2025-05-01 12:17:14 -0700

    test passing prompts

--- test_app.py
+++ test_app.py
@@ -22,3 +22,16 @@ def test_pipeline_output():
     assert type(out) is dict
     assert 'text' in out
     assert type(out['text']) is str
+
+def test_prompt_pipeline():
+    """
+    Assert that pipeline returns text transcription for input audio
+    """
+    pipe = ContextualBiasingASR_Pipeline(model_path=WHISPER_TINY, processor_path=WHISPER_TINY)
+    wav = load_and_resample(TEST_WAV)
+    chunks = chunk_audio(wav, chunk_len_s=10, return_timestamps=False)
+    prompts = ["foo bar baz"] * len(chunks)
+    out = pipe(chunks=chunks, prompts=prompts)
+    assert type(out) is dict
+    assert 'text' in out
+    assert type(out['text']) is str
